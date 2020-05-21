796ad7c5e9de74827c6152ed99d6f31eca548f73
LeeJeongSeok <ljs0429777@gmail.com>
2020-05-21 23:22:50 +0900

    add failling test

--- src/test/java/wordwrapper/WrapperTest.java
+++ src/test/java/wordwrapper/WrapperTest.java
@@ -9,6 +9,8 @@ public class WrapperTest {
     @Test
     public void should_wrap() {
         assertThat(wrap("word word", 4)).isEqualTo("word\nword");
+        assertThat(wrap("a dog", 5)).isEqualTo("a dog");
+
     }
 
     private String wrap(String s, int length) {
