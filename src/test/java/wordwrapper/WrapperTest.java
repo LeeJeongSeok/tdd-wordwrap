422ddbb505473c9cc7966d4e58533398c67d2d90
LeeJeongSeok <ljs0429777@gmail.com>
2020-05-22 00:35:08 +0900

    getting stuck

--- src/test/java/wordwrapper/WrapperTest.java
+++ src/test/java/wordwrapper/WrapperTest.java
@@ -10,10 +10,11 @@ public class WrapperTest {
     public void should_wrap() {
         assertThat(wrap("word word", 4)).isEqualTo("word\nword");
         assertThat(wrap("a dog", 5)).isEqualTo("a dog");
+        assertThat(wrap("a dog with a bone", 6)).isEqualTo("a dog\nwidth a\nbone");
 
     }
 
-    private String wrap(String s, int length) {
-        return s.replace(" ", "\n");
+    private String wrap(String s, int width) {
+        return s.length() > width ? s.replaceAll(" ", "\n") : s;
     }
 }
