3862397f4ce187ba7b53402fe548633727a06301
LeeJeongSeok <ljs0429777@gmail.com>
2020-05-22 00:57:22 +0900

    complete

--- src/test/java/wordwrapper/WrapperTest.java
+++ src/test/java/wordwrapper/WrapperTest.java
@@ -19,17 +19,20 @@ public class WrapperTest {
         assertWraps("x", 1, "x");
         assertWraps("xx", 1, "x\nx");
         assertWraps("xxx", 1, "x\nx\nx");
+        assertWraps("x x", 1, "x\nx");
+        assertWraps("four score and seven years ago our fathers brought forth upon this continent", 7, "four\nscore\nand\nseven\nyears\nago our\nfathers\nbrought\nforth\nupon\nthis\ncontine\nnt");
     }
 
     private String wrap(String s, int width) {
-        if (s == null) {
+        if (s == null)
             return "";
-        }
-
-        if (s.length() <= width) {
+        if (s.length() <= width)
             return s;
+        else {
+            int breakPoint = s.lastIndexOf(" ", width);
+            if (breakPoint == -1)
+                breakPoint = width;
+            return s.substring(0, breakPoint) + "\n" + wrap(s.substring(breakPoint).trim(), width);
         }
-
-        return s.substring(0, width) + "\n" + wrap(s.substring(width), width);
     }
 }
